72d37d6b5136c58cc97779e6f91e0cc0cd50b276
Bo Gotthardt <mail@gotthardt.bo>
2013-05-01 22:58:01 +0200

    Changed external files to be protocol-relative.

--- src/main/javascript/require.config.js
+++ src/main/javascript/require.config.js
@@ -1,9 +1,9 @@
 require.config({
     paths: {
-        jquery: "http://code.jquery.com/jquery-2.0.0.min",
-        webfont: "http://ajax.googleapis.com/ajax/libs/webfont/1/webfont",
-        //iframeapi: "http://api.viewer.zmags.com/widgets/iframe"
-        iframeapi: "http://cdn-api-viewer-a.eu.zmags.com/widgets/iframe"
+        jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js",
+        webfont: "//ajax.googleapis.com/ajax/libs/webfont/1/webfont",
+        //iframeapi: "//secure.api.viewer.zmags.com/widgets/iframe"
+        iframeapi: "//cdn-secure-api-viewer-a.eu.zmags.com/widgets/iframe"
     },
     shim: {
         webfont: {

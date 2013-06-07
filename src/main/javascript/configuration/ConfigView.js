71b76ba47d695b38e20e7a0640f4284100bc3793
Bo Gotthardt <mail@gotthardt.bo>
2013-06-07 22:32:40 +0200

    Hooked up preview view.

--- src/main/javascript/configuration/ConfigView.js
+++ src/main/javascript/configuration/ConfigView.js
@@ -1,51 +1,9 @@
 /*global window*/
-define(["jquery", "marionette", "hbars!template/AnimationConfigView", "lib/bootstrap-colorpicker"/*, "configuration/EventBus"*/],
-    function ($, Marionette, AnimationConfigView, EventBus) {
+define(["jquery", "marionette", "hbars!template/AnimationConfigView", "lib/bootstrap-colorpicker"],
+    function ($, Marionette, AnimationConfigView) {
         "use strict";
 
-        var TYPING_DELAY = 500;
-
-        function getData() {
-            /*jshint validthis:true */
-            var scope = this;
-            window.clearTimeout(this.dataTimeout);
-
-            this.dataTimeout = window.setTimeout(function () {
-                var parameters = {},
-                    requiredMissing = false;
-
-                scope.ui.formInputs.each(function (i, element) {
-                    var e = $(element);
-
-                    if (e.prop("type") === "checkbox") {
-                        if (e.prop("checked")) {
-                            if (parameters[e.data("parameter")] !== undefined) {
-                                parameters[e.data("parameter")].push(e.val());
-                            } else {
-                                parameters[e.data("parameter")] = [e.val()];
-                            }
-                        }
-                    } else {
-                        if (e.val() !== "") {
-                            var value = e.val();
-
-                            if (e.data("append")) {
-                                value = value + e.data("append");
-                            }
-
-                            parameters[e.data("parameter")] = value;
-                        } else if (e.data("required") !== undefined) {
-                            requiredMissing = true;
-                        }
-                    }
-                });
-
-                if (!requiredMissing) {
-                    console.log(parameters);
-                    //EventBus.trigger("change:config", parameters);
-                }
-            }, TYPING_DELAY);
-        }
+        var TYPING_DELAY_MS = 500;
 
         return Marionette.Layout.extend({
             ui: {
@@ -55,8 +13,12 @@ define(["jquery", "marionette", "hbars!template/AnimationConfigView", "lib/boots
                 animation: "#animation"
             },
             events: {
-                "change input[type='checkbox'], select": getData,
-                "keyup input, textarea": getData
+                "change input[type='checkbox'], select": "getData",
+                "keyup input, textarea": "getData",
+                "keyup input[data-validate-https]": function (e) {
+                    var target = $(e.currentTarget);
+                    $(target.data("validate-https")).toggle(target.val().indexOf("http://") === 0);
+                }
             },
             onRender: function () {
                 this.animation.show(new Marionette.ItemView({
@@ -64,10 +26,61 @@ define(["jquery", "marionette", "hbars!template/AnimationConfigView", "lib/boots
                 }));
 
                 // TODO
-                //$(".colorpicker").colorpicker().on("changeColor", getData);
+                //$(".colorpicker").colorpicker().on("changeColor", this.getData);
 
                 // Re-bind so formInput also includes the animation form inputs.
                 this.bindUIElements();
+            },
+
+            getData: function () {
+                var scope = this;
+                window.clearTimeout(this.dataTimeout);
+
+                this.dataTimeout = window.setTimeout(function () {
+                    var parameters = {},
+                        requiredMissing = false;
+
+                    function addParameter(element) {
+                        var name = element.data("parameter"),
+                            value = element.val();
+
+                        if (element.data("append")) {
+                            value = value + element.data("append");
+                        }
+
+                        if (parameters[name] !== undefined) {
+                            if ($.isArray(parameters[name])) {
+                                parameters[name].push(value);
+                            } else {
+                                parameters[name] = [parameters[name], value];
+                            }
+                        } else {
+                            parameters[name] = value;
+                        }
+                    }
+
+                    scope.ui.formInputs.each(function (i, element) {
+                        var e = $(element);
+
+                        if (e.prop("type") === "checkbox") {
+                            if (e.prop("checked")) {
+                                addParameter(e);
+                            }
+                        } else {
+                            if (e.val() !== "") {
+                                addParameter(e);
+                            } else if (e.data("required") !== undefined) {
+                                requiredMissing = true;
+                            }
+                        }
+                    });
+
+                    if (!requiredMissing) {
+                        console.log(parameters);
+                        //EventBus.trigger("change:config", parameters);
+                        //this.model.setParameters(parameters)
+                    }
+                }, TYPING_DELAY_MS);
             }
         });
     });
--- src/main/javascript/configuration/PreviewView.js
+++ src/main/javascript/configuration/PreviewView.js
@@ -1,8 +1,53 @@
-define(["marionette", "hbars!template/PreviewView"],
-    function (Marionette, PreviewView) {
+/*global window*/
+define(["jquery", "marionette", "hbars!template/PreviewView"],
+    function ($, Marionette, PreviewView) {
         "use strict";
 
+        var ENRICHED_MAX_LENGTH = 255;
+
         return Marionette.ItemView.extend({
-            template: PreviewView
+            template: PreviewView,
+            ui: {
+                newWindow: "#newwindow",
+                refresh: "#refresh",
+                url: "#url",
+                lengthWarning: "#warning-length",
+                iframe: "#preview-frame"
+            },
+            events: {
+                "click .select-on-click": function (e) {
+                    $(e.currentTarget).select();
+                },
+                "click #refresh": "onRefreshButtonClick"
+            },
+            onRender: function () {
+                //this.listenTo(this.model, "change", this.onUrlChange);
+                this.ui.iframe.prop("src", ""/*this.model.get("baseUrl")*/);
+            },
+
+            onUrlChange: function () {
+                var url = this.model.get("url");
+
+                this.ui.newWindow.prop("href", url);
+                this.ui.url.val(url);
+                this.ui.lengthWarning.toggle(url.length > ENRICHED_MAX_LENGTH);
+
+                var parameters = this.model.get("parameters");
+                this.sendMessage({
+                    widgetCommand: "parameters",
+                    parameters: parameters
+                });
+
+            },
+
+            onRefreshButtonClick: function () {
+                this.sendMessage({
+                    widgetCommand: "activate"
+                });
+            },
+
+            sendMessage: function (data) {
+                this.ui.iframe.contentWindow.postMessage(JSON.stringify(data), "*");
+            }
         });
     });
